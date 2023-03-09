d685482a94632dabdf70276a9d7b9dc581cfeb12
nikhea <imonikheaugbodaga@gmail.com>
2023-03-09 03:28:58 +0100

    converted to nexjs 13

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-
-const Home: NextPage = () => {
-  return <div>hello world!</div>;
-};
-
-export default Home;
