d38eb33c8ff7cd184fa00a2c7ad2226ad2974578
nikhea <imonikheaugbodaga@gmail.com>
2023-03-25 23:45:04 +0100

    change product view style

--- src/store/useView.ts
+++ src/store/useView.ts
@@ -1,11 +1,26 @@
 import { create } from "zustand";
+import { persist, createJSONStorage, devtools } from "zustand/middleware";
 
 type viewState = {
   gridStyle: boolean;
   setGridStyle: (gridStyle: boolean) => void;
 };
 
-export const useViewState = create<viewState>((set) => ({
-  gridStyle: true,
-  setGridStyle: (gridStyle) => set({ gridStyle }),
-}));
+// export const useViewState = create<viewState>((set) => ({
+//   gridStyle: true,
+//   setGridStyle: (gridStyle) => set({ gridStyle }),
+// }));
+export const useViewState = create<viewState>()(
+  devtools(
+    persist(
+      (set) => ({
+        gridStyle: true,
+        setGridStyle: (gridStyle) => set({ gridStyle }),
+      }),
+      {
+        name: "viewState-storage", // name of the item in the storage (must be unique)
+        storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+      }
+    )
+  )
+);
