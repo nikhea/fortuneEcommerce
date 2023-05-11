524af3ca9de8587a3006a1e4e521941ac0a7d184
nikhea <imonikheaugbodaga@gmail.com>
2023-05-11 06:24:20 +0100

    added newProduct seed

---
+++ src/seed/product.ts
@@ -0,0 +1,75 @@
+export const newProduct = [
+  {
+    _id: "64310ea98338e958dc1842e3",
+    category: "fashion",
+    subcategory: "men fashion",
+    name: "2 In 1 Men's Short Sleeve Shorts Set - Black",
+    description: "lorem 2 luxury bags",
+    price: 2893,
+    rating: 0,
+
+    coverPhoto:
+      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+    priceSymbol: "$",
+  },
+  {
+    _id: "64310ea98338e958dc1842e3",
+    category: "fashion",
+    subcategory: "men fashion",
+    name: "2 In 1 Men's Short Sleeve Shorts Set - Black",
+    description: "lorem 2 luxury bags",
+    price: 2893,
+    rating: 0,
+    coverPhoto:
+      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+    priceSymbol: "$",
+  },
+  {
+    _id: "64310ea98338e958dc1842e3",
+    category: "fashion",
+    subcategory: "men fashion",
+    name: "2 In 1 Men's Short Sleeve Shorts Set - Black",
+    description: "lorem 2 luxury bags",
+    price: 2893,
+    rating: 0,
+    coverPhoto:
+      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+    priceSymbol: "$",
+  },
+  {
+    _id: "64310ea98338e958dc1842e3",
+    category: "fashion",
+    subcategory: "men fashion",
+    name: "2 In 1 Men's Short Sleeve Shorts Set - Black",
+    description: "lorem 2 luxury bags",
+    price: 2893,
+    rating: 0,
+    coverPhoto:
+      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+    priceSymbol: "$",
+  },
+  {
+    _id: "64310ea98338e958dc1842e3",
+    category: "fashion",
+    subcategory: "men fashion",
+    name: "2 In 1 Men's Short Sleeve Shorts Set - Black",
+    description: "lorem 2 luxury bags",
+    price: 2893,
+    rating: 0,
+    coverPhoto:
+      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+    priceSymbol: "$",
+  },
+  {
+    _id: "64310ea98338e958dc1842e3",
+    category: "fashion",
+    subcategory: "men fashion",
+    name: "2 In 1 Men's Short Sleeve Shorts Set - Black",
+    description: "lorem 2 luxury bags",
+    price: 2893,
+    rating: 0,
+    coverPhoto:
+      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+    priceSymbol: "$",
+  },
+];
