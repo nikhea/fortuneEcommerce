848928f9aced8ff76e52cfe71e2f00480004af01
nikhea <imonikheaugbodaga@gmail.com>
2023-10-08 02:19:05 +0100

    added render

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { storage } from "../auth/utils";
 
-let main = "https://backendfortuneecommerce-production.up.railway.app/api/";
+let main = "https://hekto-v4oa.onrender.com/api/";
 let test = "http://localhost:4000/api/";
 const instance = axios.create({
   baseURL: main,
