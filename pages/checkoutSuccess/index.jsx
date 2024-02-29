a3d46f7117a9caf0cd5fc2fc2c9abf696b38cea8
nikhea <imonikheaugbodaga@gmail.com>
2024-02-29 12:14:03 +0100

    wip: cart login

--- pages/checkoutSuccess/index.jsx
+++ pages/checkoutSuccess/index.jsx
@@ -5,15 +5,15 @@ import Button from "../../src/components/FormElement/Button/Button";
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 const Success = () => {
   return (
-    <div className="container h-screen flex justify-center items-center">
+    <div className="container flex items-center justify-center h-screen">
       <div className=" shadow-2xl  rounded-xl p-10 h-[80%]  ">
-        <div className=" h-full flex flex-col justify-center items-center text-center">
-          <CheckCircleIcon className=" h-36 w-36 mr-2 text-primary" />
+        <div className="flex flex-col items-center justify-center h-full text-center ">
+          <CheckCircleIcon className="mr-2  h-36 w-36 text-primary" />
 
           <h1 className="mb-2 capitalize text-[#151875] font-bold text-2xl md:text-3xl">
             your payment is successful
           </h1>
-          <p className="md:px-40 text-gray-500">
+          <p className="text-gray-500 md:px-40">
             Thank you for your order! Your order is being processed and will be
             completed within 3-6 hours. You will receive a notification when
             your order is completed.
@@ -21,7 +21,7 @@ const Success = () => {
           <Link
             href={PagesRoutes.order}
             shallow
-            className="mt-3 capitalize text-gray-500 underline hover:text-gray-600"
+            className="mt-3 text-gray-500 underline capitalize hover:text-gray-600"
           >
             {/* <Button isCurve primary padding uppercase> */}
             view orders
